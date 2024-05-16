01ed2cc3229b9ef77017c938da10a879e756a533
Kurmanbek <kurmanbekk.edu@gmail.com>
2024-05-16 10:43:29 -0700

    filtered

--- index.py
+++ index.py
@@ -2,11 +2,6 @@ from pyOfferUp import fetch
 from flask import Flask, jsonify
 import json
 
-# app.py
-
-from flask import Flask, jsonify
-import requests
-
 app = Flask(__name__)
 
 # Define OfferUp API endpoint
@@ -15,14 +10,14 @@ def get_offerup_posts():
     filteredArray = []
     try:
         # Make a GET request to the OfferUp API
-        posts = fetch.get_listings(query="Toyota Prius", state="California", city="Los Angeles", limit=30)
+        posts = fetch.get_listings(query="samsung 55", state="California", city="Los Angeles", limit=30)
         # Define a lambda function to filter out listings with a price over $300
-        # for post in posts:
-        #     price = int(post["price"])
-        #     if price <= 200:
-        #         filteredArray.append(post)
-        # filtered_posts = json.dumps(filteredArray)
-        return posts
+        for post in posts:
+            price = int(post["price"])
+            if price <= 150:
+                filteredArray.append(post)
+        filtered_posts = json.dumps(filteredArray)
+        return filtered_posts
         # Return the data as JSON
     except Exception as e:
         # Return an error message if something went wrong
