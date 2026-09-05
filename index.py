ebdf83a5e84ca20d2bee2e6a4d6bb5d60a57171b
Kurmanbek Karaev <57332034+kumaskano1-edu@users.noreply.github.com>
2026-09-05 11:20:22 -0700

    Update search queries and price range in get_offerup_posts

--- index.py
+++ index.py
@@ -9,10 +9,10 @@ def get_offerup_posts():
     combined_results = {}
     try:
         # Multiple search queries
-        search_queries = ["samsung 65", "lg 65", "samsung 75 tv", "lg 75 tv", "oled lg", "qled tv",]
+        search_queries = ["samsung 75 tv", "lg 75", "lg 70", "oled 65", "lg oled 65",]
 
         min_price = request.args.get('min_price', default=100, type=float)
-        max_price = request.args.get('max_price', default=200, type=float)
+        max_price = request.args.get('max_price', default=300, type=float)
 
         for query in search_queries:
             posts = fetch.get_listings(query=query, state="California", city="Los Angeles", limit=50)
